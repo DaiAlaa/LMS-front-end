99a43d86f0f759e57fd5ac77151eb1261e5b95e7
Nori <nerdeen.ahmad15@gmail.com>
2021-12-29 01:20:51 +0200

    course work starting

--- src/modules/Course.js
+++ src/modules/Course.js
@@ -99,9 +99,9 @@ export default {
           console.log(error);
         });
     },
-    getCourse({commit}){
+    getCourse({commit},course_id){
       axios
-      .get(urlRequest + "")
+      .get(urlRequest + "/courses/?id="+course_id)
       .then((response) => {
         let Course = response.data;
         commit("getCourse", Course);
